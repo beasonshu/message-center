fa9904fab18ea3ba6bfce2af0f61c1c023360472
shu.xinghu <shu.xinghu@jinyinmao.com.cn>
2018-06-27 09:34:11 +0800

    Initial commit

---
+++ src/main/java/cn/com/jinyinmao/messagecenter/MessageCenterApplication.java
@@ -0,0 +1,20 @@
+package cn.com.jinyinmao.messagecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class MessageCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageCenterApplication.class, args);
+	}
+
+	@RequestMapping(value = "/",produces = "text/plain;charset=UTF-8")
+	public String index(){
+		return "Hello Spring Boot";
+	}
+}
